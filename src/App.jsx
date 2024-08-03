5dd94a898db831571ccd13102c09dcfd853d5a38
kungumasakthivel <kungumasakthivel.k@gmail.com>
2024-08-03 15:10:27 +0530

    addNote post method created

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,14 @@ import Home from './pages/Home/Home'
 import Login from './pages/Login/Login'
 import Register from './pages/Register/Register'
 import { Routes, Route } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
 
   return (
     <>
+      <ToastContainer/>
       <Routes>
         <Route path='/' element={<Register/>} />
         <Route path='/login' element={<Login/>} />
--- src/pages/CRUD/Crud.jsx
+++ src/pages/CRUD/Crud.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React, {useState} from 'react'
+import './Crud.css'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
 
 const Crud = () => {
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
+  const nav = useNavigate()
+  
+  // post method 
+  const addNote = async() => {
+    const item = {title, body};
+    const token = localStorage.getItem('token')
+    if(!token){
+      nav('/login')
+    }
+    const result = await fetch('https://claw-backend-hrmw.onrender.com/crud/create', {
+      method: 'POST',
+      body: JSON.stringify(item),
+      headers: {
+        "Authorization": token,
+        "Content-Type": 'application/json',
+        "Accept": 'application/json'
+      }
+    })
+    const responce = await result.json();
+    console.log(responce)
+    const status = responce.status
+    if(status === 1) {
+      toast.success("Created successfully");
+      setTitle('');
+      setBody('');
+    } else if(status === 0) {
+      toast.error("Please enter a valid data");
+    }
+    
+  }
+
   return (
     <div className='crud-container'>
-      <h1>Create Read Update Delete</h1>
+      <h1>CRUD</h1>
+      <div className='input-crud-container'> 
+        <input type="text" placeholder="Title" 
+        className='input-crud' value={title} 
+        onChange={(e) => setTitle(e.target.value)}
+        />
+        <input type="text" placeholder='Body'
+        className='input-crud' value={body}
+        onChange={(e) => {setBody(e.target.value)}}
+        />
+        <button className='button' onClick={addNote}>Add</button>
+      </div>
     </div>
   )
 }
